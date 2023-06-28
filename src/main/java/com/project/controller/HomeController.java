47a8bfeae957fa2cb5765ad2dfaba0c9b80361d6
skingsol <skingsol@naver.com>
2023-06-28 12:06:09 +0900

    홈컨트롤러 미사용임포트 수정

--- src/main/java/com/project/controller/HomeController.java
+++ src/main/java/com/project/controller/HomeController.java
@@ -3,21 +3,15 @@ package com.project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 import com.project.domain.SearchDTO;
 import com.project.naver.dto.WishListDTO;
 import com.project.naver.service.WishListService;
-import com.project.service.SearchService;
 
 import lombok.extern.slf4j.Slf4j;
 
