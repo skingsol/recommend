a135aae222d31824ced947d5baf01f44ee163799
smj <amjey@naver.com>
2023-06-30 17:12:31 +0900

    필요 없는 import 삭제

--- src/main/java/com/project/controller/ApiController.java
+++ src/main/java/com/project/controller/ApiController.java
@@ -2,12 +2,11 @@ package com.project.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.project.domain.RegisterRstrntDTO;
 import com.project.ms.dto.BringNaverApiDTO;
--- src/main/java/com/project/controller/HomeController.java
+++ src/main/java/com/project/controller/HomeController.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.project.domain.RegisterRstrntDTO;
 import com.project.ms.dto.BringNaverApiDTO;
 import com.project.service.BringNaverService;
 import com.project.service.RegisterRstrntService;
